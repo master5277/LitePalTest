09d3934a188eadee24bb1b86e646d6d33757b9c8
2324 <2324139312@qq.com>
2024-05-18 12:05:37 +0800

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # LitePalTest
+
+导入依赖时出错：
+
+```
+android studio引入 implementation 'org.litepal.guolindev:core:3.2.3'出错，
+报错信息：
+
+What went wrong:
+Execution failed for task ':app:checkDebugAarMetadata'.
+Could not resolve all files for configuration ':app:debugRuntimeClasspath'.
+Could not find org.litepal.guolindev:core:3.2.3.
+Searched in the following locations:
+
+as版本：Android Studio Giraffe | 2022.3.1 Patch 3
+用的gradle脚本
+是什么原因啊？
+```
+
+解决方法：
+
+```
+在gradle脚本的setting.gradle文件的dependencyResolutionManagement>repositories选项内添加：
+
+jcenter()
+maven { url 'https://jitpack.io' }
+
+sync一下就好了
+参考链接：https://www.yii666.com/blog/330831.html
+```
+
+这样就可以正常使用依赖了。
+
+第一行代码书籍中的代码有些过时，具体参考：
+
+```
+https://github.com/guolindev/LitePal
+```
+
+其中更换的有：
+
+```
+public class Book extends LitePalSupport 
+```
+
+```
+LitePal.getDatabase();
+```
+
+```
+                LitePal.deleteAll(Book.class,"price < ?","15");
+```
+
+```
+  List<Book> books1 = LitePal.select("name","author","pages")
+                        .where("pages > ?","400")
+                        .order("pages")
+                        .limit(4)
+                        .offset(1)
+                        .find(Book.class);
+```
+
--- app/src/main/java/com/example/litepaltest/MainActivity.java
+++ app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.litepaltest;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,92 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //更新表信息方法1
+//                Book book = new Book();
+//                book.setName("The Lost Symbol");
+//                book.setAuthor("Dan Brown");
+//                book.setPages(510);
+//                book.setPrice(19.95);
+//                book.setPress("Unknow");
+//                book.save();
+//                book.setPrice(10.99);
+//                book.save();
+
+                //更新表信息方法2
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?","The Lost Symbol","Dan Brown");
+
+            }
+        });
+
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.deleteAll(Book.class,"price < ?","15");
+            }
+        });
+
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Book> books = LitePal.findAll(Book.class);
+                for (Book book:books)
+                {
+                    Log.d("MainActivity", "book name is :"+book.getName());
+                }
+                Log.d("MainActivity", "onClick: ");
+                List<Book> books1 = LitePal.select("name","author","pages")
+                        .where("pages > ?","400")
+                        .order("pages")
+                        .limit(4)
+                        .offset(1)
+                        .find(Book.class);
+                for (Book book:books1)
+                {
+                    Log.d("MainActivity", "The book name is :"+book.getName());
+                }
+            }
         });
     }
 }
